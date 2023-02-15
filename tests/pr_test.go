5b40264effcd6d7550fd16a0d21de5acb8b08b65
Daniel Butler <dannyeb@gmail.com>
2023-02-15 15:57:41 +0000

    test: remove old variables
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,13 +2,11 @@
 package test
 
 import (
-	"log"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
-	"gopkg.in/yaml.v2"
 )
 
 // Use existing resource group
@@ -21,34 +19,8 @@ const autoscalingExampleTerraformDir = "examples/redis-auto-scaling"
 // Restricting due to limited availability of BYOK in certain regions
 const regionSelectionPath = "../common-dev-assets/common-go-assets/icd-region-prefs.yaml"
 
-// Define a struct with fields that match the structure of the YAML data
-const yamlLocation = "../common-dev-assets/common-go-assets/common-permanent-resources.yaml"
-
-type Config struct {
-	SmGuid   string `yaml:"secretsManagerGuid"`
-	SmRegion string `yaml:"secretsManagerRegion"`
-}
-
-var smGuid string
-var smRegion string
-
 // TestMain will be run before any parallel tests, used to read data from yaml for use with tests
 func TestMain(m *testing.M) {
-	// Read the YAML file contents
-	data, err := os.ReadFile(yamlLocation)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Create a struct to hold the YAML data
-	var config Config
-	// Unmarshal the YAML data into the struct
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// Parse the SM guid and region from data
-	smGuid = config.SmGuid
-	smRegion = config.SmRegion
 	os.Exit(m.Run())
 }
 
@@ -109,9 +81,7 @@ func testRunComplete(t *testing.T, version string) {
 		BestRegionYAMLPath: regionSelectionPath,
 
 		TerraformVars: map[string]interface{}{
-			"redis_version":               version,
-			"existing_sm_instance_guid":   smGuid,
-			"existing_sm_instance_region": smRegion,
+			"redis_version": version,
 		},
 	})
 
@@ -138,10 +108,6 @@ func TestRunUpgradeExample(t *testing.T) {
 		Prefix:             "redis-template-upg",
 		ResourceGroup:      resourceGroup,
 		BestRegionYAMLPath: regionSelectionPath,
-		TerraformVars: map[string]interface{}{
-			"existing_sm_instance_guid":   smGuid,
-			"existing_sm_instance_region": smRegion,
-		},
 	})
 
 	output, err := options.RunTestUpgrade()
