eac416fcc0ead5c1be1a74a517ece4458a108a85
Daniel Butler <dannyeb@gmail.com>
2023-02-15 15:26:36 +0000

    test: skip upgrade for initial migration
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -128,6 +128,8 @@ func TestRunCompleteExample(t *testing.T) {
 }
 
 func TestRunUpgradeExample(t *testing.T) {
+	// TODO: Remove this line after the first merge to primary branch is complete to enable upgrade test
+	t.Skip("Skipping upgrade test until initial code is in primary branch")
 	t.Parallel()
 
 	options := testhelper.TestOptionsDefaultWithVars(&testhelper.TestOptions{
