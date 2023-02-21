ca44fd74cf3a435c603b720771ee85b006924748
Daniel Butler <dannyeb@gmail.com>
2023-02-21 16:54:36 +0000

    fix: remove provider for sm
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- examples/complete/provider.tf
+++ examples/complete/provider.tf
@@ -19,29 +19,3 @@ provider "restapi" {
     Content-Type     = "application/vnd.ibm.kms.policy+json"
   }
 }
-
-
-provider "restapi" {
-  alias                = "sm"
-  uri                  = "https:"
-  write_returns_object = true
-  debug                = false
-  headers = {
-    Authorization = data.ibm_iam_auth_token.token_data.iam_access_token
-    Content-Type  = "application/json"
-  }
-}
-
-# RestAPI provider using alternate configuration to be used by SM module
-provider "restapi" {
-  alias                 = "nocontent"
-  uri                   = "https:"
-  write_returns_object  = false
-  create_returns_object = false
-  debug                 = false # set to true to show detailed logs, but use carefully as it might print sensitive values.
-  headers = {
-    Accept        = "application/json"
-    Authorization = data.ibm_iam_auth_token.token_data.iam_access_token
-    Content-Type  = "application/json"
-  }
-}
