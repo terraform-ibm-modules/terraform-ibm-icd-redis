b136db7e25193940252812eda5644e0c4f10305c
Daniel Butler <dannyeb@gmail.com>
2023-02-23 11:17:45 +0000

    fix: output guid
    Signed-off-by: Daniel Butler <dannyeb@gmail.com>

--- profiles/fscloud/outputs.tf
+++ profiles/fscloud/outputs.tf
@@ -7,6 +7,13 @@ output "id" {
   value       = module.redis.id
 }
 
+
+output "guid" {
+  description = "redis instance guid"
+  value       = module.redis.guid
+}
+
+
 output "version" {
   description = "redis instance version"
   value       = module.redis.version
