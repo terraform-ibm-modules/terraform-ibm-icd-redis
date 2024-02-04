6b5f5978fa79866d06ad6b7672426faf4b372568
Steve Peggs <peggs@uk.ibm.com>
2024-02-04 21:38:56 +0000

    fix: inputs and outputs on kms

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -40,7 +40,16 @@ module "key_protect_all_inclusive" {
   region                    = var.region
   key_protect_instance_name = "${var.prefix}-kp"
   resource_tags             = var.resource_tags
-  key_map                   = { "icd" = ["${var.prefix}-redis"] }
+  keys = [
+    {
+      key_ring_name = "icd"
+      keys = [
+        {
+          key_name = "${var.prefix}-redis"
+        }
+      ]
+    }
+  ]
 }
 
 ##############################################################################
@@ -78,7 +87,7 @@ module "icd_redis" {
   region                     = var.region
   admin_pass                 = var.admin_pass
   users                      = var.users
-  existing_kms_instance_guid = module.key_protect_all_inclusive.key_protect_guid
+  existing_kms_instance_guid = module.key_protect_all_inclusive.kms_guid
   kms_key_crn                = module.key_protect_all_inclusive.keys["icd.${var.prefix}-redis"].crn
   tags                       = var.resource_tags
   service_credential_names   = var.service_credential_names
