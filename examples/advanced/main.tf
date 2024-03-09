d6686362e44e0236d6bc3526922a882338deca0b
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2024-03-09 02:49:24 +0000

    chore(deps): update terraform terraform-ibm-modules/key-protect-all-inclusive/ibm to v4.8.3

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -33,7 +33,7 @@ resource "ibm_is_subnet" "testacc_subnet" {
 
 module "key_protect_all_inclusive" {
   source            = "terraform-ibm-modules/key-protect-all-inclusive/ibm"
-  version           = "4.8.2"
+  version           = "4.8.3"
   resource_group_id = module.resource_group.resource_group_id
   # Note: Database instance and Key Protect must be created in the same region when using BYOK
   # See https://cloud.ibm.com/docs/cloud-databases?topic=cloud-databases-key-protect&interface=ui#key-byok
