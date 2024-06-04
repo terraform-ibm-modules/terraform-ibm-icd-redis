5529d6d9f0b48d135182001f2902ed033a98ceb2
Conall Ó Cofaigh <ocofaigh@ie.ibm.com>
2024-06-04 11:04:35 +0100

    Update main.tf

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -42,10 +42,12 @@ module "key_protect_all_inclusive" {
   resource_tags             = var.resource_tags
   keys = [
     {
-      key_ring_name = "icd"
+      key_ring_name         = "icd"
+      force_delete_key_ring = true
       keys = [
         {
-          key_name = "${var.prefix}-redis"
+          key_name     = "${var.prefix}-redis"
+          force_delete = true
         }
       ]
     }
