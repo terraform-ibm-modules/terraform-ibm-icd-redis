a6ed8cd331c6d70f91b52dfcae024d35ead899bb
Aditya Ranjan <48973656+Aditya-ranjan-16@users.noreply.github.com>
2024-11-26 16:27:22 +0530

    feat: exposed the ability to set the `provider_visibility` in the DA [Learn more](https://registry.terraform.io/providers/IBM-Cloud/IBM/latest/docs#visibility-1) (#463)

--- solutions/standard/variables.tf
+++ solutions/standard/variables.tf
@@ -227,3 +227,13 @@ variable "backup_crn" {
     error_message = "backup_crn must be null OR starts with 'crn:' and contains ':backup:'"
   }
 }
+variable "provider_visibility" {
+  description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
+  type        = string
+  default     = "private"
+
+  validation {
+    condition     = contains(["public", "private", "public-and-private"], var.provider_visibility)
+    error_message = "Invalid visibility option. Allowed values are 'public', 'private', or 'public-and-private'."
+  }
+}
