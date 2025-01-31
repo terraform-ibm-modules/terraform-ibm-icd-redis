a4e04b6eb462d6fdc851f42f71a0022a33c3d301
shemau <shemau@gmail.com>
2025-01-31 14:51:30 +0000

    feat: updated the `service_credential_secrets` input object schema of the DA to support creating service specific roles<br>* The `service_credentials_source_service_role` attribute has been renamed to `service_credentials_source_service_role_crn` and now requires a full CRN value. For example: `service_credentials_source_service_role_crn = "crn:v1:bluemix:public:iam::::role:Editor" (#510)
    Co-authored-by: Conall Ó Cofaigh <ocofaigh@ie.ibm.com>

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -130,15 +130,15 @@ func TestRunStandardSolutionSchematics(t *testing.T) {
 
 	serviceCredentialSecrets := []map[string]interface{}{
 		{
-			"secret_group_name": fmt.Sprintf("%s-secret-group", prefix),
+			"secret_group_name": fmt.Sprintf("%s-secret-group", options.Prefix),
 			"service_credentials": []map[string]string{
 				{
-					"secret_name": fmt.Sprintf("%s-cred-reader", prefix),
-					"service_credentials_source_service_role": "Reader",
+					"secret_name": fmt.Sprintf("%s-cred-reader", options.Prefix),
+					"service_credentials_source_service_role_crn": "crn:v1:bluemix:public:iam::::role:Viewer",
 				},
 				{
-					"secret_name": fmt.Sprintf("%s-cred-writer", prefix),
-					"service_credentials_source_service_role": "Writer",
+					"secret_name": fmt.Sprintf("%s-cred-writer", options.Prefix),
+					"service_credentials_source_service_role_crn": "crn:v1:bluemix:public:iam::::role:Editor",
 				},
 			},
 		},
