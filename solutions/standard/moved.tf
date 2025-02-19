a9b511b1430250e4fa759e7d385c4a3147c7e5e5
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2025-02-20 01:20:14 +0530

    add: moved block

--- solutions/standard/moved.tf
+++ solutions/standard/moved.tf
@@ -12,3 +12,8 @@ moved {
   from = module.redis.module.redis.time_sleep.wait_for_authorization_policy[0]
   to   = module.redis[0].module.redis.time_sleep.wait_for_authorization_policy[0]
 }
+
+moved {
+  from = module.redis.module.redis.ibm_resource_tag.access_tag[0]
+  to   = module.redis[0].module.redis.ibm_resource_tag.access_tag[0]
+}
