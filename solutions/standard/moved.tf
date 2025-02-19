63aba015fcf03005097b74d7bf922244aa06b80b
Aayush-Abhyarthi <Aayush.Abhyarthi@ibm.com>
2025-02-20 00:17:56 +0530

    add: moved block

---
+++ solutions/standard/moved.tf
@@ -0,0 +1,14 @@
+moved {
+  from = module.redis.module.redis.ibm_database.redis_database
+  to   = module.redis[0].module.redis.ibm_database.redis_database
+}
+
+moved {
+  from = module.redis.module.redis.ibm_iam_authorization_policy.kms_policy[0]
+  to   = module.redis[0].module.redis.ibm_iam_authorization_policy.kms_policy[0]
+}
+
+moved {
+  from = module.redis.module.redis.time_sleep.wait_for_authorization_policy[0]
+  to   = module.redis[0].module.redis.time_sleep.wait_for_authorization_policy[0]
+}
