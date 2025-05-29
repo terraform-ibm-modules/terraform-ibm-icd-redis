01cf441b1236c96720cdfe4952141a7a57a24bd0
akocbek <akocbek>
2025-05-29 12:20:14 +0100

    fix tests

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -115,6 +115,7 @@ func TestRunFullyConfigurableSolutionSchematics(t *testing.T) {
 		{Name: "admin_pass_secrets_manager_secret_name", Value: options.Prefix, DataType: "string"},
 		{Name: "prefix", Value: options.Prefix, DataType: "string"},
 		{Name: "admin_pass", Value: GetRandomAdminPassword(t), DataType: "string"},
+		{Name: "use_ibm_owned_encryption_key", Value: true, DataType: "bool"},
 	}
 	err = options.RunSchematicTest()
 	assert.Nil(t, err, "This should not have errored")
