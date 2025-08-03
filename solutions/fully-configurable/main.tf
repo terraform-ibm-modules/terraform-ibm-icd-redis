b4d68680b118e37997d5ce1c02dd37384d1eaee4
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-08-03 20:12:40 +0100

    fix(deps): update terraform terraform-ibm-modules/kms-all-inclusive/ibm to v5.1.16 (#633)

--- solutions/fully-configurable/main.tf
+++ solutions/fully-configurable/main.tf
@@ -32,7 +32,7 @@ module "kms" {
   }
   count                       = local.create_new_kms_key ? 1 : 0
   source                      = "terraform-ibm-modules/kms-all-inclusive/ibm"
-  version                     = "5.1.15"
+  version                     = "5.1.16"
   create_key_protect_instance = false
   region                      = local.kms_region
   existing_kms_instance_crn   = var.existing_kms_instance_crn
