a124bf50973599fd2695a73ea8de3fb3049cb9ae
Md Anam Raihan <md.anam.raihan@ibm.com>
2026-06-16 11:02:39 +0530

    resolve comments

--- main.tf
+++ main.tf
@@ -320,7 +320,7 @@ resource "ibm_database" "redis_database" {
 
 # Check whether access tags are valid and exist in the account
 data "ibm_iam_access_tag" "access_tags" {
-  for_each = length(var.access_tags) != 0 ? toset(var.access_tags) : []
+  for_each = toset(var.access_tags)
   name     = each.value
 }
 
